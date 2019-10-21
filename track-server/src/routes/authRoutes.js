181d74066b691b12935c009214ff73f0bf234c61
SHANbic <supagoof924@hotmail.com>
2019-10-21 20:58:26 +0200

    [ FEATURE ] Now able to save a user on post request

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-    console.log(req.body)
-  res.send("post request fdp");
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  const user = new User({ email, password });
+  await user.save();
+  res.send('User Created')
 });
 
 module.exports = router;
