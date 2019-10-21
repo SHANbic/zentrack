224d18a5cc2640d28b9bb7ce610f559e9169f0d7
SHANbic <supagoof924@hotmail.com>
2019-10-21 20:29:16 +0200

    [ CONFIG ] Created a route handler file

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -3,15 +3,14 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const keys = require("../env");
 const app = express();
+const authRoutes = require("./routes/authRoutes");
 
 mongoose.connect(keys.MONGOURI, {
   useNewUrlParser: true,
   useCreateIndex: true
 });
 
-app.get("/", (req, res) => {
-  res.send("hi there");
-});
+app.use(authRoutes);
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to mongo instance");
