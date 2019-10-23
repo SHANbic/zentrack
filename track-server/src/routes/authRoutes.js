00d7c07976b8dddf2fce98a99712b178a4f3dd4d
SHANbic <supagoof924@hotmail.com>
2019-10-23 21:34:11 +0200

    [ UPDATE ] Now able to save and retrieve user in DB

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,21 +1,42 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const User = mongoose.model("User");
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = mongoose.model('User');
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
+router.post('/signup', async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = new User({ email, password });
     await user.save();
 
-    const token = jwt.sign({ userId: user._id }, "my-secret-key");
+    const token = jwt.sign({ userId: user._id }, 'my-secret-key');
     res.send({ token });
   } catch (err) {
     res.status(422).send(err.message);
+  }
+});
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide both email and password' });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).send({ error: 'Invalid email or password' });
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'my-secret-key');
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid email or password' });
   }
 });
 
