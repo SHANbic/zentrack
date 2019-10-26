3f93321caaaf77b3a497a8e912dbbce695deb0d2
SHANbic <supagoof924@hotmail.com>
2019-10-26 11:28:12 +0200

    [ FEATURE ] Made navigation effective with button throughout the app

--- track-client/src/screens/SigninScreen.js
+++ track-client/src/screens/SigninScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
