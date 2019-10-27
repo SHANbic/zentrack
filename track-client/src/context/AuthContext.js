dae0eaaf795b337c56affa40964141f60957884c
SHANbic <supagoof924@hotmail.com>
2019-10-27 11:08:07 +0100

    [ FEATURE ] Implemented auth into front end signup screen

--- track-client/src/context/AuthContext.js
+++ track-client/src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -16,10 +17,13 @@ const signIn = dispatch => {
 };
 
 const signUp = dispatch => {
-  return ({ email, password }) => {
-    //api request
-    //success => modify state
-    //failure => error message
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post('/signup', { email, password });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err.response.data);
+    }
   };
 };
 
