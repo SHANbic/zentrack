ad24cc0d8796efbb9bf0c13fb2944b1048387bfa
SHANbic <supagoof924@hotmail.com>
2019-10-27 11:07:27 +0100

    [ CONFIG ] Added api request handler 

---
+++ track-client/src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL : 'http://46837bf6.ngrok.io'
+})
--- track-client/src/context/AuthContext.js
+++ track-client/src/context/AuthContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signIn = dispatch => {
+  return ({ email, password }) => {
+    //api request
+    //success => modify state
+    //failure => error message
+  };
+};
+
+const signUp = dispatch => {
+  return ({ email, password }) => {
+    //api request
+    //success => modify state
+    //failure => error message
+  };
+};
+
+const signOut = dispatch => {
+  return () => {
+    // kill token
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signIn, signOut, signUp },
   { isSignedIn: false }
 );
