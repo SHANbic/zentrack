2c0a23648f7d4422d730ec4f012bce1d95d4fcaf
SHANbic <supagoof924@hotmail.com>
2019-10-28 21:28:14 +0100

    [ REFACTO ] Made use of reusable components to display sign up screen

--- track-client/src/screens/SignupScreen.js
+++ track-client/src/screens/SignupScreen.js
@@ -1,47 +1,25 @@
-import React, { useState, useContext } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import { Spacer, HorCen } from '../components/Spacer';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
 import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SignupScreen = ({ navigation }) => {
   const { state, signUp } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <AuthForm
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signUp}
       />
-      <Spacer />
-      <Input
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-        secureTextEntry
+      <NavLink
+        text="Already have an account?"
+        buttonText="Sign in"
+        routeName="Signin"
       />
-      {state.errorMessage ? (
-        <Text style={styles.error}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signUp({ email, password })} />
-      </Spacer>
-      <HorCen>
-        <Text>Already have an account?</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-          <Text style={styles.link}>Sign in</Text>
-        </TouchableOpacity>
-      </HorCen>
     </View>
   );
 };
@@ -57,15 +35,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 250
-  },
-  error: {
-    color: 'red',
-    fontSize: 16,
-    marginLeft: 15,
-    marginTop: 15
-  },
-  link: {
-    color: '#4284FF'
   }
 });
 
