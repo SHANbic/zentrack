4bee79989b16a201e29c74d247c12f7cfc28add2
SHANbic <supagoof924@hotmail.com>
2019-10-30 20:25:08 +0100

    [ FEATURE ] Handled offscreen text positionning and created a Map component

--- track-client/src/screens/AccountScreen.js
+++ track-client/src/screens/AccountScreen.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
 import { Button, Text } from 'react-native-elements';
 import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountScreen = () => {
   const { signOut } = useContext(AuthContext);
   return (
-    <>
+    <SafeAreaView forceInset={{ top: 'always' }}>
       <Text style={{ fontSize: 48 }}>AccountScreen</Text>
       <Button title="Sign Out" onPress={signOut} />
-    </>
+    </SafeAreaView>
   );
 };
 
