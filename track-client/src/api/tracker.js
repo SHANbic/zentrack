e32ae65c850824fa01e431a60167b32a0ff097c2
SHANbic <supagoof924@hotmail.com>
2019-11-05 21:50:24 +0100

    [ FEATURE ] Able to save a track, empty datas and redict to tracklistscreen

--- track-client/src/api/tracker.js
+++ track-client/src/api/tracker.js
@@ -7,7 +7,7 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   async config => {
-    const token = await AsyncStorage.get('token');
+    const token = await AsyncStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
